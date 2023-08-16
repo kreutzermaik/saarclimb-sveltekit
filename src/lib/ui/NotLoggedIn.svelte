e7ba3ff13099ec249ffdef843774dc79697bda48
Maik Kreutzer <kreutzermaik123@web.de>
2023-08-16 12:54:38 +0200

    Planer component layout

---
+++ src/lib/ui/NotLoggedIn.svelte
@@ -0,0 +1,30 @@
+<main class="text-center mx-auto text-gray-700 p-4">
+  <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+    <svg
+      class="w-2/3 mx-auto text-error"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+    <div class="card-body">
+      <h2 class="card-title">Für diese Seite musst du eingeloggt sein!</h2>
+      <div class="card-actions justify-center py-4">
+        <button class="btn btn-error">
+          <a
+            href="/login"
+            class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >Zum Login</a>
+        </button>
+      </div>
+    </div>
+  </div>
+</main>
--- src/routes/plan/+page.svelte
+++ src/routes/plan/+page.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
+  import Planer from "$lib/features/Planer.svelte";
   import Header from "../../lib/features/Header.svelte";
+  import Session from "../../session";
+  import NotLoggedIn from "$lib/ui/NotLoggedIn.svelte";
 
+  let isLoggedIn: boolean = false;
+
+  /**
+   * initialize isLoggedIn variable
+   */
+   async function initLoggedIn() {
+    try {
+      isLoggedIn = await Session.isLoggedIn();
+    } catch (err: any) {
+      console.log(err);
+    }
+  }
+
+  initLoggedIn();
 </script>
 
 <svelte:head>
@@ -9,3 +26,13 @@
 </svelte:head>
 
 <Header headerText="Wochenplaner" />
+
+{#if isLoggedIn}
+  <div class="card card-compact shadow-xl bg-white">
+    <div class="card-body">
+      <Planer />
+    </div>
+  </div>
+{:else}
+  <NotLoggedIn />
+{/if}
