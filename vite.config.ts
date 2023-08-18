19962c8e0a9441fdae4a371d240fc21811b24ed0
Maik Kreutzer <kreutzermaik123@web.de>
2023-08-18 12:31:46 +0200

    added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [
+        sveltekit(),
+        VitePWA({
+            manifest: {
+                icons: [
+                    {
+                        src: "logo.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    }
+                ]
+            }
+        })
+    ]
 });
