aae1bdd4dea8256c48bcb093fa20d826d51e695c
Maik Kreutzer <kreutzermaik123@web.de>
2023-09-08 10:27:32 +0200

    test something

--- src/supabase.ts
+++ src/supabase.ts
@@ -12,9 +12,11 @@ if (dev) {
     supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 }
 
-if (!supabaseUrl || supabaseUrl == '' || supabaseUrl == undefined) {
-    supabaseUrl = 'https://ybeongwjjfdkgizzkmsc.supabase.co';
-    supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliZW9uZ3dqamZka2dpenprbXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI4MjMzMzQsImV4cCI6MTk4ODM5OTMzNH0.NaSZptwqEuuAfhKmoCV_lHraA8nROlQmwKCxEHc8z34';
-}
+/*if (!supabaseUrl || supabaseUrl == '' || supabaseUrl == undefined) {
+    supabaseUrl = 'https://tqgufceactcswwkbjfjk.supabase.co';
+    supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZ3VmY2VhY3Rjc3d3a2JqZmprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA2OTMyOTksImV4cCI6MTk4NjI2OTI5OX0.MRYORIJPYAwuRfISOKKsKqCnfABJqLaXT1cZQZ6ZAE4';
+}*/
+
+console.log(supabaseUrl)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
