d6815fe433070ab351622f8bd5e384a40974a8b2
Maik Kreutzer <s17ce7@umwelt-campus.de>
2023-12-09 14:31:03 +0100

    updated ci script

--- cypress/e2e/performance.cy.ts
+++ cypress/e2e/performance.cy.ts
@@ -3,20 +3,6 @@
 describe('progress spec', () => {
     it('passes', () => {
         /**
-         * Login
-         */
-        cy.visit('/');
-        cy.wait(2000);
-        // cy.get('#goto-login').should('be.visible').click();
-        cy.wait(1000);
-        // cy.get('#email-field').should('be.visible').type('cypress@user.com');
-        cy.wait(1000);
-        // cy.get('#password-field').should('be.visible').type('test123');
-        cy.wait(1000);
-        // cy.get('#login button').should('be.visible').click();
-        cy.wait(1000);
-
-        /**
          * Progress
          */
         cy.visit('/progress');
