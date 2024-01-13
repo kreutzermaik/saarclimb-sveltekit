fe1950ef8e13b9ffe21269f8dc90f46d5ebd14f4
Maik Kreutzer <s17ce7@umwelt-campus.de>
2024-01-13 07:36:43 +0100

    Baseline und Leerlauf Messungen

---
+++ cypress/e2e/performance-leerlauf.cy.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('performance leerlauf spec', () => {
+    it('passes', () => {
+        cy.wait(60000);
+    })
+})
--- cypress/e2e/performance.cy.ts
+++ cypress/e2e/performance.cy.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('progress spec', () => {
+describe('performance spec', () => {
     it('passes', () => {
         /**
          * Progress
